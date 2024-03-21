5de4773db3977ea0f87441ad7416c9cdbcc176b5
harold18m <harold.medrano@tecsup.edu.pe>
2024-03-20 23:56:23 -0500

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ContactosComponent } from "./contactos/contactos.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, ContactosComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'semana-01';
